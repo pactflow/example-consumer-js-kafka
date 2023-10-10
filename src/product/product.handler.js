d5fc8f0263e1a1e40e5d3f989292b3dfdd033bde
Yousaf Nabi <yousaf.nabi@smartbear.com>
2023-10-10 23:29:53 +0100

    chore: update deps (#36)
    * chore: update deps
    
    * ci: update to node 16
    
    * ci: update ci tags
    
    * chore: update to publish with branches
    
    * chore: revert back to tags until provider updated

--- src/product/product.handler.js
+++ src/product/product.handler.js
@@ -4,7 +4,7 @@ const Product = require('./product');
 const handler = (product) => {
   console.log('received product:', product)
 
-  Promise.resolve(repository.insert(new Product(product.id, product.type, product.name, product.version)))
+  return Promise.resolve(repository.insert(new Product(product.id, product.type, product.name, product.version)))
 }
 
 module.exports = handler
